3e85c73563d2e77aaa05ca7d3efa0b8098311aed
Parikshit Mali <parikshitmali17@gmail.com>
2025-03-05 16:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 //your JS code here. If required.
-let text= document.getElementById("text").value;
-let number=document.getElementById("number").value;
+
+
+document.getElementById("btn").addEventListner('click',()=>{
+	let text= document.getElementById("text").value;
+let delay=document.getElementById("delay").value;
 let output=document.getElementById("output");
 
-async function display(){
+	if(!delay || !text){
+		output.innerText="Please Enter Text and Delay";
+		return
+	}
+
+	delay=parseInt(delay);
+
+	await new Promise((resolve)=>{
+		setTimeout(()=resolve,delay*1000)
+		output.innerText=text
+	})
+	
+})async function display(){
 	return await setTimeout(()=>{output.value=text},number*1000)
 }
