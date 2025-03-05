ff4a0b897f09c05296b2c81673c5bd312f5bb4ac
Parikshit Mali <parikshitmali17@gmail.com>
2025-03-05 15:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let text= document.getElementById("text").value;
+let number=document.getElementById("number").value;
+let output=document.getElementById("output");
+
+async function display(){
+	return await setTimeout(()=>>{output.appedChild(text)},number*1000)
+}
